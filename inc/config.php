97ad4c6aad3050eee765530efa3ac95736c3d914
Gerson Borges <passupreto@gmail.com>
2019-09-18 14:08:19 -0300

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+header("Content-Type: text/html; charset=utf8");
+
+define("SERVIDOR","mysql:host=localhost;dbname=jequiti;charset=utf8");
+define("USUARIO","root");
+define("SENHA","");
