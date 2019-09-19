551bbd24d877c924abae8141010cded208f6f567
Gerson Borges <passupreto@gmail.com>
2019-09-19 07:57:19 -0300

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+<!--Footer-->
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">© 2019 Copyright:
+        <a href="http://plixsite.net" target="_blank"> PlixSite.Net </a>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
+
+<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="js/popper.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/mdb.min.js"></script>
+<script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+
+</script>
+</body>
+
+</html>
