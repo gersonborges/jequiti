6bc72d52e6dd1534b165f5df396a052e1ec7d426
Gerson Borges <passupreto@gmail.com>
2019-09-19 07:56:02 -0300

    Update Produto_all.php

--- view/Produto_all.php
+++ view/Produto_all.php
@@ -1,52 +1,153 @@
-<?php //include 'inc/header.php'; ?>
-<h2>Produtos</h2>
-
-<?php
-// se tem uma mensagem de erro mostra a mensagem e em seguida apaga
-if( isset($_SESSION['msg'])){
-    echo $_SESSION['msg'];
-    unset($_SESSION['msg']);
-}
-?>
-
-
+<?php include 'inc/header.php'; ?>
 
 <br>
 <br>
+<br>
 
-<table class="table">
-
-    <tr>
-        <th>Produto</th>
-        <th>Categoria</th>
-        <th>Fornecedor</th>
-    </tr>
+<!--Main layout-->
+<main>
+    <div class="container">
 
 
-    <?php foreach($produtos AS $produto){ ?>
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark mdb-color lighten-3 mt-3 mb-5">
 
-        <tr>
+            <span class="navbar-brand">Categorias:</span>
 
-            <td><?=$produto->nome?></td>
-            <td><?=$produto->categoria_nome?></td>
-            <td><?=$produto->fornecedor_nome?></td>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+                    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        </tr>
+            <div class="collapse navbar-collapse" id="basicExampleNav">
 
-    <?php } ?>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Todas
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
 
-</table>
+                    <?php foreach($categorias as $categoria){ ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><?=$categoria->nome?></a>
+                        </li>
+                    <?php } ?>
 
-<?php //include 'inc/footer.php'; ?>
+                </ul>
+
+                <form class="form-inline">
+                    <div class="md-form my-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
+                    </div>
+                </form>
+            </div>
+
+        </nav>
+        <!--/.Navbar-->
+
+        <!--Section: Products v.3-->
+        <section class="text-center mb-4">
+
+            <!--Grid row-->
+            <div class="row wow fadeIn">
+
+                <?php foreach($produtos as $produto){ ?>
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4">
+
+                        <!--Card-->
+                        <div class="card">
+
+                            <!--Card image-->
+                            <div class="view overlay">
+                                <img src="<?=$produto->foto?>" class="card-img-top"
+                                     alt="">
+                                <a>
+                                    <div class="mask rgba-white-slight"></div>
+                                </a>
+                            </div>
+                            <!--Card image-->
+
+                            <!--Card content-->
+                            <div class="card-body text-center">
+                                <!--Category & Title-->
+                                <a href="" class="grey-text">
+                                    <h5><?=$produto->categoria_nome?></h5>
+                                </a>
+                                <h5>
+                                    <strong>
+                                        <a href="" class="dark-grey-text"><?=$produto->nome?>
+                                            <span class="badge badge-pill danger-color">NOVO</span>
+                                        </a>
+                                    </strong>
+                                </h5>
+
+                                <h4 class="font-weight-bold blue-text">
+                                    <strong>R$<?=number_format($produto->preco_venda,2,",",".")?></strong>
+                                </h4>
+
+                            </div>
+                            <!--Card content-->
+
+                        </div>
+                        <!--Card-->
+
+                    </div>
+                    <!--Grid column-->
+                <?php } ?>
 
 
 
 
-<script>
-    $('#myModal').on('shown.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var id = button.data('id');
-        $("#btn-excluir").attr('href','?classe=Disciplina&acao=delete&id='+id);
 
-    });
-</script>
+            </div>
+            <!--Grid row-->
+
+        </section>
+        <!--Section: Products v.3-->
+
+        <!--Pagination-->
+        <nav class="d-flex justify-content-center wow fadeIn">
+            <ul class="pagination pg-blue">
+
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                </li>
+
+                <li class="page-item active">
+                    <a class="page-link" href="#">1
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">4</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">5</a>
+                </li>
+
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!--Pagination-->
+
+    </div>
+</main>
+<!--Main layout-->
+
+<?php include 'inc/footer.php'; ?>
